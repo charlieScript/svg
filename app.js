c613324f41fe8897b9ae55fe8aeb317a129b5da2
charliescript <gozione3@gmail.com>
2020-08-27 16:35:28 +0100

    done

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const logo = document.querySelectorAll('#logo path')
 
-// logo.forEach((i) => {
-//   console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
-// })
-
 console.log(logo);
 
 for (let i = 0; i < logo.length; i++) {
   console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
+  logo[i].style.strokeDashArray = logo[i].getTotalLength()
+  logo[i].style.strokeDashOffset = logo[i].getTotalLength()
+  // console.log(logo[i].style.strokeDashOffset);
 }
+
+
+logo.forEach(function(i) {
+  console.log(parseInt(i.getTotalLength()));
+})
